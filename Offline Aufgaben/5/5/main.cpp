4a5cdfaaa5660b9a14bb67fcd090272fab2b2869
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-12-06 15:20:27 +0100

    Offline Aufgabe 5

---
+++ Offline Aufgaben/5/5/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numArr[9] = {};
+	int counter = 0;
+
+	for (int i = 0; i < 9; i++) {
+		char c = ' ';
+		while (c < '0' || c > '6') {
+			cout << "Bitte geben Sie die " << i + 1 << ". Zahl ein: ? ";
+			cin >> c;
+		}
+		numArr[i] = c - '0';
+
+		bool unique = true;
+		for (int j = 0; j < i && unique; j++) {
+			if (numArr[i] == numArr[j]) unique = false;
+		}
+
+		if (unique) counter++;
+	}
+
+	cout << "In der Eingabe kamen " << counter << " unterschiedliche Zahlen vor." << endl;
+
+	return 0;
+}
---
+++ Offline Aufgaben/5/9/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string trimme(string input) {
+	string output = "";
+	string buffer = "";
+	
+	for (int i = 0; i < input.length(); i++) {
+		if (output.length() == 0 && input[i] == '+') continue;
+
+		if (input[i] == '+') {
+			buffer += '+';
+			continue;
+		}
+
+		output += buffer + input[i];
+	}
+
+	return output;
+}
+
+int main() {
+	string s = "";
+
+	cout << "Bitte geben Sie die Textzeile ein: ? ";
+	getline(cin, s);
+
+	cout << "Vorher: " << s << endl;
+	cout << "Nachher: " << trimme(s) << endl;
+	
+	return 0;
+}
